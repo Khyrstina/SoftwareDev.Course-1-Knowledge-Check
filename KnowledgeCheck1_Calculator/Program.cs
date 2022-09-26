b3e01d5306a6f072ae5b38bdb112b9788f7c62d8
Khyrstina Smith <khyrstinasmith@gmail.com>
2022-09-26 13:56:31 -0400

    Moved the console.readline method and variables Number1/Number2
    Tested code, found that window was closing before you could see result.
    
    Added Console.WriteLine to keep window open until user presses enter key.

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -4,17 +4,17 @@ Console.WriteLine("Hello. Press 1 for addition, 2 for subtraction, 3 for multipl
 
 var input = Console.ReadLine();
 var calculator = new Calculator();
+{ 
+    Console.WriteLine("Enter 2 integers to solve for");
+var Number1 = Console.ReadLine();
+var Number2 = Console.ReadLine();
 
 switch (input)
 {
     case "1":
-        Console.WriteLine("Enter 2 integers to add");
-        var addNumber1 = Console.ReadLine();
-        var addNumber2 = Console.ReadLine();
-
-        if (int.TryParse(addNumber1, out int addNumOne) && int.TryParse(addNumber2, out int addNumTwo))
+        if (int.TryParse(Number1, out int addNumOne) && int.TryParse(Number2, out int addNumTwo))
         {
-            Console.Write($"{addNumber1} + {addNumber2} = ");
+            Console.Write($"{Number1} + {Number2} =  ");
             Console.Write(calculator.Add(addNumOne, addNumTwo));
         }
         else
@@ -24,13 +24,9 @@ switch (input)
         break;
 
     case "2":
-        Console.WriteLine("Enter 2 integers to subtract");
-        var subtractNumber1 = Console.ReadLine();
-        var subtractNumber2 = Console.ReadLine();
-
-        if (int.TryParse(subtractNumber1, out int subNumOne) && int.TryParse(subtractNumber2, out int subNumTwo))
+ if (int.TryParse(Number1, out int subNumOne) && int.TryParse(Number2, out int subNumTwo))
         {
-            Console.Write($"{subtractNumber1} - {subtractNumber2} = ");
+            Console.Write($"{Number1} - {Number2} =  ");
             Console.Write(calculator.Subtract(subNumOne, subNumTwo));
         }
         else
@@ -40,13 +36,9 @@ switch (input)
         break;
 
     case "3":
-        Console.WriteLine("Enter 2 integers to multiply");
-        var multiplyNumber1 = Console.ReadLine();
-        var multiplyNumber2 = Console.ReadLine();
-
-        if (int.TryParse(multiplyNumber1, out int mulNumOne) && int.TryParse(multiplyNumber2, out int mulNumTwo))
+if (int.TryParse(Number1, out int mulNumOne) && int.TryParse(Number2, out int mulNumTwo))
         {
-            Console.Write($"{multiplyNumber1} * {multiplyNumber2} =");
+            Console.Write($"{Number1} * {Number2} =  ");
             Console.Write(calculator.Multiply(mulNumOne, mulNumTwo));
         }
         else
@@ -56,13 +48,9 @@ switch (input)
         break;
 
     case "4":
-        Console.WriteLine("Enter 2 integers to divide");
-        var divideNumber1 = Console.ReadLine();
-        var divideNumber2 = Console.ReadLine();
-
-        if (double.TryParse(divideNumber1, out double divNumOne) && double.TryParse(divideNumber2, out double divNumTwo))
+if (double.TryParse(Number1, out double divNumOne) && double.TryParse(Number2, out double divNumTwo))
         {
-            Console.Write($"{divideNumber1} / {divideNumber2} = ");
+            Console.Write($"{Number1} / {Number2} =  ");
             Console.Write(calculator.Divide(divNumOne, divNumTwo));
         }
         else
@@ -76,5 +64,8 @@ switch (input)
         Console.WriteLine("Unknown input");
         break;
 
-
+}
+    Console.WriteLine();
+        Console.WriteLine("Press enter to close...");
+    Console.ReadLine();
 }
